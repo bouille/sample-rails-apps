9997779bdc9c0c51784a31595c8ada58722341d9
August Lilleaas <augustlilleaas@gmail.com>
2008-11-27 19:52:02 +0100

    Bumping jquery_and_ajax to 2.2

--- jquery_and_ajax/config/routes.rb
+++ jquery_and_ajax/config/routes.rb
@@ -43,6 +43,8 @@ ActionController::Routing::Routes.draw do |map|
 
   # Install the default routes as the lowest priority.
   # No, we don't.
+  # Note: These default routes make all actions in every controller accessible via GET requests. You should
+  # consider removing the them or commenting them out if you're using named routes and resources.
   # map.connect ':controller/:action/:id'
   # map.connect ':controller/:action/:id.:format'
 end
